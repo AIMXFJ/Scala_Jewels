d70cab8592fc87628b37154875f0321c75976ac5
AIMX <hectoraimx@gmail.com>
2017-03-21 16:24:18 +0100

    Analisis Manual implementado

--- .worksheet/src/jewels_worksheet.scala
+++ .worksheet/src/jewels_worksheet.scala
@@ -15,44 +15,121 @@ object jewels_worksheet {
   	if(dificultad == 1)(1+Random.nextInt(3))
   	else if(dificultad == 2)(1+Random.nextInt(5))
   	else if(dificultad == 3)(1+Random.nextInt(7))
-  	else 0;System.out.println("""numRandom: (dificultad: Int)Int""");$skip(280); 
-  
+  	else 0;System.out.println("""numRandom: (dificultad: Int)Int""");$skip(277); 
   
   //Inicializa el tablero de "times" tamaño a un numero aleatorio en función de la dificultad
   def initTablero (times:Int, dificultad:Int) : List[Int] = {
  		times match { case 0 => Nil
  		case times => initTablero (times-1,dificultad):::numRandom(dificultad)::Nil
  		}
-	};System.out.println("""initTablero: (times: Int, dificultad: Int)List[Int]""");$skip(159); 
+	};System.out.println("""initTablero: (times: Int, dificultad: Int)List[Int]""");$skip(366); 
 	
+	//Numero de jewels a la izquierda de la posicion (x,y)
+	def cuantasJewelsIzquierda(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x-1+(y)*anchura >= 0)
+			if(tablero(x-1+(y)*anchura)==valor)
+				cuantasJewelsIzquierda(tablero, anchura, altura, x-1, y, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	};System.out.println("""cuantasJewelsIzquierda: (tablero: List[Int], anchura: Int, altura: Int, x: Int, y: Int, valor: Int, contador: Int)Int""");$skip(372); 
 	
-	//Analiza si la jewel que se ha movido a la posicion de destino permite eliminar jewels, cuantas y cuales
-	def analisisManual(tablero:List[Int]):Unit = 0;System.out.println("""analisisManual: (tablero: List[Int])Unit""");$skip(258); 
+	//Numero de jewels a la derecha de la posicion (x,y)
+	def cuantasJewelsDerecha(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x+1+(y)*anchura < anchura*altura)
+			if(tablero(x+1+(y)*anchura)==valor)
+				cuantasJewelsDerecha(tablero, anchura, altura, x+1, y, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	};System.out.println("""cuantasJewelsDerecha: (tablero: List[Int], anchura: Int, altura: Int, x: Int, y: Int, valor: Int, contador: Int)Int""");$skip(354); 
 	
+	//Numero de jewels por debajo de la posicion (x,y)
+	def cuantasJewelsAbajo(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x+(y-1)*anchura >= 0)
+			if(tablero(x+(y-1)*anchura)==valor)
+				cuantasJewelsAbajo(tablero, anchura, altura, x, y-1, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	};System.out.println("""cuantasJewelsAbajo: (tablero: List[Int], anchura: Int, altura: Int, x: Int, y: Int, valor: Int, contador: Int)Int""");$skip(368); 
+	
+	//Numero de jewels por encima de la posicion (x,y)
+	def cuantasJewelsArriba(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x+(y+1)*anchura < anchura*altura)
+			if(tablero(x+(y+1)*anchura)==valor)
+				cuantasJewelsArriba(tablero, anchura, altura, x, y+1, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	};System.out.println("""cuantasJewelsArriba: (tablero: List[Int], anchura: Int, altura: Int, x: Int, y: Int, valor: Int, contador: Int)Int""");$skip(456); 
+	
+	//Devuelve las jewels a eliminar en Vertical como List
+	def llenarJewelsEliminarVertical(tablero:List[Int], anchura:Int, x:Int, y:Int, contArriba:Int, contAbajo:Int):List[Int] =
+		if(contArriba>0) llenarJewelsEliminarVertical(tablero,anchura,x,y+1,contArriba-1,contAbajo):::tablero(x + (y+1) * anchura)::Nil
+		else if(contAbajo>0) llenarJewelsEliminarVertical(tablero,anchura,x,y-1,contArriba,contAbajo-1):::tablero(x + (y-1) * anchura)::Nil
+		else Nil;System.out.println("""llenarJewelsEliminarVertical: (tablero: List[Int], anchura: Int, x: Int, y: Int, contArriba: Int, contAbajo: Int)List[Int]""");$skip(488); 
+	
+	//Devuelve las jewels a eliminar en Horizontal como List
+	def llenarJewelsEliminarHorizontal(tablero:List[Int], anchura:Int, x:Int, y:Int, contDerecha:Int, contIzquierda:Int):List[Int] =
+		if(contDerecha>0) llenarJewelsEliminarHorizontal(tablero,anchura,x+1,y,contDerecha-1,contIzquierda):::tablero(x + 1 + (y) * anchura)::Nil
+		else if(contIzquierda>0) llenarJewelsEliminarHorizontal(tablero,anchura,x-1,y,contDerecha,contIzquierda-1):::tablero(x - 1 + (y) * anchura)::Nil
+		else Nil;System.out.println("""llenarJewelsEliminarHorizontal: (tablero: List[Int], anchura: Int, x: Int, y: Int, contDerecha: Int, contIzquierda: Int)List[Int]""");$skip(913); 
+	
+	//Analiza si hay jewels para eliminar y devuelve cuales en caso de que se pueda eliminar
+	def analizarPosicion(tablero:List[Int],anchura:Int,altura:Int,x:Int,y:Int):List[Int] = {
+		if(cuantasJewelsDerecha(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) + cuantasJewelsIzquierda(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) >= 3){
+			llenarJewelsEliminarHorizontal(tablero,anchura,x,y,cuantasJewelsDerecha(tablero,anchura,altura,x,y,tablero(x+y*anchura),0),cuantasJewelsIzquierda(tablero,anchura,altura,x,y,tablero(x+y*anchura),0))
+		}else if(cuantasJewelsArriba(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) + cuantasJewelsAbajo(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) >= 3){
+			llenarJewelsEliminarVertical(tablero,anchura,x,y,cuantasJewelsArriba(tablero,anchura,altura,x,y,tablero(x+y*anchura),0),cuantasJewelsAbajo(tablero,anchura,altura,x,y,tablero(x+y*anchura),0))
+		}else Nil
+	};System.out.println("""analizarPosicion: (tablero: List[Int], anchura: Int, altura: Int, x: Int, y: Int)List[Int]""");$skip(197); 
+	
+	//TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+	def eliminarJewels(tablero:List[Int], anchura:Int, altura:Int, dificultad:Int, jewelsParaEliminar:List[Int]):List[Int] = tablero;System.out.println("""eliminarJewels: (tablero: List[Int], anchura: Int, altura: Int, dificultad: Int, jewelsParaEliminar: List[Int])List[Int]""");$skip(509); 
+	
+	//Analiza si la jewel que se ha movido a la posicion de destino permite eliminar jewels, cuantas y cuales y llama a eliminarlas
+	def analisisManual(tablero:List[Int], dificultad:Int,anchura:Int, altura:Int, x:Int, y:Int, valor:Int):List[Int] = {
+		val jewelsParaEliminar = analizarPosicion(tablero, anchura, altura, x, y)
+		if(jewelsParaEliminar == Nil) {/*return tablero*/ jewelsParaEliminar}
+		else {
+			//eliminarJewels(tablero,anchura,altura,dificultad,jewelsParaEliminar)
+			jewelsParaEliminar
+		}
+	};System.out.println("""analisisManual: (tablero: List[Int], dificultad: Int, anchura: Int, altura: Int, x: Int, y: Int, valor: Int)List[Int]""");$skip(256); 
 	
 	//Intercambia 2 elementos en el tablero, para ello se necesita el primer elemento y ambas posiciones
 	def swap(tablero:List[Int], pos1:Int, pos2:Int, elem1:Int):List[Int] =
-		introducirElemento(elem1,pos2,introducirElemento(tablero(pos2),pos1,tablero));System.out.println("""swap: (tablero: List[Int], pos1: Int, pos2: Int, elem1: Int)List[Int]""");$skip(791); 
+		introducirElemento(elem1,pos2,introducirElemento(tablero(pos2),pos1,tablero));System.out.println("""swap: (tablero: List[Int], pos1: Int, pos2: Int, elem1: Int)List[Int]""");$skip(1236); 
 	
 	//Funcion que se ocupa de intercambiar con la jewel indicada y realizar llamar al analisis manual
 	def intercambiarJewels(tablero:List[Int], pos1_x:Int, pos1_y:Int, direccion:Int, anchura:Int, altura:Int, seleccion:Int):List[Int] = {
 		direccion match {
 		//Arriba
-			case 1 => swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y+1)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 1 => { swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y+1)*anchura, tablero(pos1_x + pos1_y*anchura))
+									analisisManual(tablero, dificultad,anchura, altura, pos1_x, pos1_y+1, tablero(pos1_x + pos1_y*anchura)) }
 		//Abajo
-			case 2 => swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y-1)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 2 => { swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y-1)*anchura, tablero(pos1_x + pos1_y*anchura))
+									analisisManual(tablero, dificultad,anchura, altura, pos1_x, pos1_y-1, tablero(pos1_x + pos1_y*anchura)) }
 		//Izquierda
-			case 3 => swap(tablero, pos1_x + pos1_y*anchura, pos1_x-1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 3 => { swap(tablero, pos1_x + pos1_y*anchura, pos1_x-1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+								analisisManual(tablero, dificultad,anchura, altura, pos1_x+1, pos1_y, tablero(pos1_x + pos1_y*anchura)) }
 		//Derecha
-			case 4=> swap(tablero, pos1_x + pos1_y*anchura, pos1_x+1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 4=> { swap(tablero, pos1_x + pos1_y*anchura, pos1_x+1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+								analisisManual(tablero, dificultad,anchura, altura, pos1_x+1, pos1_y, tablero(pos1_x + pos1_y*anchura)) }
 		}
-		//analisisManual()
 	};System.out.println("""intercambiarJewels: (tablero: List[Int], pos1_x: Int, pos1_y: Int, direccion: Int, anchura: Int, altura: Int, seleccion: Int)List[Int]""");$skip(21); 
   
 	val anchura = 15;System.out.println("""anchura  : Int = """ + $show(anchura ));$skip(17); 
-	val altura = 10;System.out.println("""altura  : Int = """ + $show(altura ));$skip(20); 
+	val altura = 11;System.out.println("""altura  : Int = """ + $show(altura ));$skip(20); 
 	val dificultad = 3;System.out.println("""dificultad  : Int = """ + $show(dificultad ));$skip(84); 
 	
 	//Se inicializa el tablero
-	val tablero = initTablero(anchura*altura,dificultad);System.out.println("""tablero  : List[Int] = """ + $show(tablero ))}
+	val tablero = initTablero(anchura*altura,dificultad);System.out.println("""tablero  : List[Int] = """ + $show(tablero ));$skip(80); val res$0 = 
+	
+	analisisManual(tablero,dificultad,anchura,altura,5,7,tablero(5 + 7*anchura));System.out.println("""res0: List[Int] = """ + $show(res$0))}
 }
--- src/jewels_worksheet.sc
+++ src/jewels_worksheet.sc
@@ -5,66 +5,131 @@ object jewels_worksheet {
 	def introducirElemento(color:Int, pos:Int, tablero:List[Int]):List[Int] =
 		if(tablero.isEmpty) Nil else if (pos==0) color::tablero.tail
 		else tablero.head::introducirElemento(color,pos-1,tablero.tail)
-                                                  //> introducirElemento: (color: Int, pos: Int, tablero: List[Int])List[Int]
   
   def printTablero(tablero:List[Int], pos:Int, anchura:Int, altura:Int):Unit =
   	if(pos%anchura==0 && pos!=0) println("")
-  	else println(tablero(pos))                //> printTablero: (tablero: List[Int], pos: Int, anchura: Int, altura: Int)Unit
-                                                  //| 
+  	else println(tablero(pos))
   
   //Genera un numero aleatorio en funcion de la dificultad
   def numRandom(dificultad:Int): Int =
   	if(dificultad == 1)(1+Random.nextInt(3))
   	else if(dificultad == 2)(1+Random.nextInt(5))
   	else if(dificultad == 3)(1+Random.nextInt(7))
-  	else 0                                    //> numRandom: (dificultad: Int)Int
-  
+  	else 0
   
   //Inicializa el tablero de "times" tamaño a un numero aleatorio en función de la dificultad
   def initTablero (times:Int, dificultad:Int) : List[Int] = {
  		times match { case 0 => Nil
  		case times => initTablero (times-1,dificultad):::numRandom(dificultad)::Nil
  		}
-	}                                         //> initTablero: (times: Int, dificultad: Int)List[Int]
+	}
 	
+	//Numero de jewels a la izquierda de la posicion (x,y)
+	def cuantasJewelsIzquierda(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x-1+(y)*anchura >= 0)
+			if(tablero(x-1+(y)*anchura)==valor)
+				cuantasJewelsIzquierda(tablero, anchura, altura, x-1, y, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	}
 	
-	//Analiza si la jewel que se ha movido a la posicion de destino permite eliminar jewels, cuantas y cuales
-	def analisisManual(tablero:List[Int]):Unit = 0
-                                                  //> analisisManual: (tablero: List[Int])Unit
+	//Numero de jewels a la derecha de la posicion (x,y)
+	def cuantasJewelsDerecha(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x+1+(y)*anchura < anchura*altura)
+			if(tablero(x+1+(y)*anchura)==valor)
+				cuantasJewelsDerecha(tablero, anchura, altura, x+1, y, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	}
 	
+	//Numero de jewels por debajo de la posicion (x,y)
+	def cuantasJewelsAbajo(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x+(y-1)*anchura >= 0)
+			if(tablero(x+(y-1)*anchura)==valor)
+				cuantasJewelsAbajo(tablero, anchura, altura, x, y-1, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	}
+	
+	//Numero de jewels por encima de la posicion (x,y)
+	def cuantasJewelsArriba(tablero:List[Int], anchura:Int, altura:Int, x:Int, y:Int, valor:Int, contador:Int):Int = {
+		if(x+(y+1)*anchura < anchura*altura)
+			if(tablero(x+(y+1)*anchura)==valor)
+				cuantasJewelsArriba(tablero, anchura, altura, x, y+1, valor, contador+1)
+			else
+				contador
+		else
+			contador
+	}
+	
+	//Devuelve las jewels a eliminar en Vertical como List
+	def llenarJewelsEliminarVertical(tablero:List[Int], anchura:Int, x:Int, y:Int, contArriba:Int, contAbajo:Int):List[Int] =
+		if(contArriba>0) llenarJewelsEliminarVertical(tablero,anchura,x,y+1,contArriba-1,contAbajo):::tablero(x + (y+1) * anchura)::Nil
+		else if(contAbajo>0) llenarJewelsEliminarVertical(tablero,anchura,x,y-1,contArriba,contAbajo-1):::tablero(x + (y-1) * anchura)::Nil
+		else Nil
+	
+	//Devuelve las jewels a eliminar en Horizontal como List
+	def llenarJewelsEliminarHorizontal(tablero:List[Int], anchura:Int, x:Int, y:Int, contDerecha:Int, contIzquierda:Int):List[Int] =
+		if(contDerecha>0) llenarJewelsEliminarHorizontal(tablero,anchura,x+1,y,contDerecha-1,contIzquierda):::tablero(x + 1 + (y) * anchura)::Nil
+		else if(contIzquierda>0) llenarJewelsEliminarHorizontal(tablero,anchura,x-1,y,contDerecha,contIzquierda-1):::tablero(x - 1 + (y) * anchura)::Nil
+		else Nil
+	
+	//Analiza si hay jewels para eliminar y devuelve cuales en caso de que se pueda eliminar
+	def analizarPosicion(tablero:List[Int],anchura:Int,altura:Int,x:Int,y:Int):List[Int] = {
+		if(cuantasJewelsDerecha(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) + cuantasJewelsIzquierda(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) >= 3){
+			llenarJewelsEliminarHorizontal(tablero,anchura,x,y,cuantasJewelsDerecha(tablero,anchura,altura,x,y,tablero(x+y*anchura),0),cuantasJewelsIzquierda(tablero,anchura,altura,x,y,tablero(x+y*anchura),0))
+		}else if(cuantasJewelsArriba(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) + cuantasJewelsAbajo(tablero,anchura,altura,x,y,tablero(x+y*anchura),0) >= 3){
+			llenarJewelsEliminarVertical(tablero,anchura,x,y,cuantasJewelsArriba(tablero,anchura,altura,x,y,tablero(x+y*anchura),0),cuantasJewelsAbajo(tablero,anchura,altura,x,y,tablero(x+y*anchura),0))
+		}else Nil
+	}
+	
+	//TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+	def eliminarJewels(tablero:List[Int], anchura:Int, altura:Int, dificultad:Int, jewelsParaEliminar:List[Int]):List[Int] = tablero
+	
+	//Analiza si la jewel que se ha movido a la posicion de destino permite eliminar jewels, cuantas y cuales y llama a eliminarlas
+	def analisisManual(tablero:List[Int], dificultad:Int,anchura:Int, altura:Int, x:Int, y:Int, valor:Int):List[Int] = {
+		val jewelsParaEliminar = analizarPosicion(tablero, anchura, altura, x, y)
+		if(jewelsParaEliminar == Nil) {/*return tablero*/ jewelsParaEliminar}
+		else {
+			//eliminarJewels(tablero,anchura,altura,dificultad,jewelsParaEliminar)
+			jewelsParaEliminar
+		}
+	}
 	
 	//Intercambia 2 elementos en el tablero, para ello se necesita el primer elemento y ambas posiciones
 	def swap(tablero:List[Int], pos1:Int, pos2:Int, elem1:Int):List[Int] =
 		introducirElemento(elem1,pos2,introducirElemento(tablero(pos2),pos1,tablero))
-                                                  //> swap: (tablero: List[Int], pos1: Int, pos2: Int, elem1: Int)List[Int]
 	
 	//Funcion que se ocupa de intercambiar con la jewel indicada y realizar llamar al analisis manual
 	def intercambiarJewels(tablero:List[Int], pos1_x:Int, pos1_y:Int, direccion:Int, anchura:Int, altura:Int, seleccion:Int):List[Int] = {
 		direccion match {
 		//Arriba
-			case 1 => swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y+1)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 1 => { swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y+1)*anchura, tablero(pos1_x + pos1_y*anchura))
+									analisisManual(tablero, dificultad,anchura, altura, pos1_x, pos1_y+1, tablero(pos1_x + pos1_y*anchura)) }
 		//Abajo
-			case 2 => swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y-1)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 2 => { swap(tablero, pos1_x + pos1_y*anchura, pos1_x + (pos1_y-1)*anchura, tablero(pos1_x + pos1_y*anchura))
+									analisisManual(tablero, dificultad,anchura, altura, pos1_x, pos1_y-1, tablero(pos1_x + pos1_y*anchura)) }
 		//Izquierda
-			case 3 => swap(tablero, pos1_x + pos1_y*anchura, pos1_x-1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 3 => { swap(tablero, pos1_x + pos1_y*anchura, pos1_x-1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+								analisisManual(tablero, dificultad,anchura, altura, pos1_x+1, pos1_y, tablero(pos1_x + pos1_y*anchura)) }
 		//Derecha
-			case 4=> swap(tablero, pos1_x + pos1_y*anchura, pos1_x+1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+			case 4=> { swap(tablero, pos1_x + pos1_y*anchura, pos1_x+1 + (pos1_y)*anchura, tablero(pos1_x + pos1_y*anchura))
+								analisisManual(tablero, dificultad,anchura, altura, pos1_x+1, pos1_y, tablero(pos1_x + pos1_y*anchura)) }
 		}
-		//analisisManual()
-	}                                         //> intercambiarJewels: (tablero: List[Int], pos1_x: Int, pos1_y: Int, direccio
-                                                  //| n: Int, anchura: Int, altura: Int, seleccion: Int)List[Int]
+	}
   
-	val anchura = 15                          //> anchura  : Int = 15
-	val altura = 10                           //> altura  : Int = 10
-	val dificultad = 3                        //> dificultad  : Int = 3
+	val anchura = 15
+	val altura = 11
+	val dificultad = 3
 	
 	//Se inicializa el tablero
 	val tablero = initTablero(anchura*altura,dificultad)
-                                                  //> tablero  : List[Int] = List(7, 5, 5, 2, 6, 3, 2, 6, 4, 7, 3, 7, 5, 5, 5, 6,
-                                                  //|  1, 6, 6, 4, 5, 7, 5, 7, 2, 7, 3, 4, 5, 4, 1, 2, 5, 5, 2, 7, 7, 3, 7, 3, 1,
-                                                  //|  2, 5, 7, 5, 5, 3, 4, 1, 3, 1, 1, 4, 3, 7, 6, 4, 2, 4, 5, 1, 3, 4, 7, 3, 5,
-                                                  //|  6, 7, 6, 2, 7, 7, 2, 6, 2, 5, 4, 6, 2, 1, 7, 7, 5, 1, 5, 7, 5, 2, 2, 3, 5,
-                                                  //|  3, 3, 1, 3, 4, 1, 3, 7, 2, 3, 4, 5, 7, 5, 4, 3, 3, 6, 4, 5, 5, 2, 5, 2, 7,
-                                                  //|  2, 3, 1, 1, 1, 7, 5, 5, 7, 6, 4, 6, 6, 6, 4, 7, 6, 6, 3, 5, 2, 3, 6, 5, 3,
-                                                  //|  5, 6, 5, 7, 3, 3, 5, 7, 2)
+	
+	analisisManual(tablero,dificultad,anchura,altura,5,7,tablero(5 + 7*anchura))
 }
